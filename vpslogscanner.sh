a22ccd1ca81a78e9567ff347629fde5f6b966121
slrslr <czautohits@gmail.com>
2016-06-05 14:40:37 +0200

    Update vpslogscanner.sh
    Nginx, Webmin, Apache2 logs now should be read too
    Unable fork error is muted in case one of the VPS is crashed

--- vpslogscanner.sh
+++ vpslogscanner.sh
@@ -1,18 +1,18 @@
-# empty old report file
+# Empty report file populated in previous run
 > /tmp/allvpslogscanneroutput
 
-# Script tails webserver access logs on all VPSs using VZCTL (openvz tool) and send out report to admin using tool "mutt" (make sure it is installed)
+# This script tail webserver access logs on all VPSs using VZCTL OpenVZ utility and create report fille which can be sent via email and coppied to the public webserver path for admin to check
+# If you are unable or do not wish to host resulting report on the OVZ server or any of its VPS, comment out line below line "# Create WebPage Out Of Log File command"# If you are unable or do not wish to host resulting report on the OVZ server or any of its VPS, comment out line below line "# Create WebPage Out Of Log File command", if you do wish to have web accessible report of all VPS access logs, then edit that line
 
 # exclude CTIDs
-exclude="MYVPSID|1234|65432"
-adminmail=adminmailhere@gmail.com
-# adjust also line 52 and 56
+exclude="860|1234|65432"
+adminmail=adminmail@gmail.com
 
 ########### cPanel acccess logs
 
 for ctid in $(vzlist -Ho ctid|grep -vE "$exclude");do
 
-cmd="echo \"----------- CTID $ctid IP $(vzlist $ctid -Ho ip) cPanel access logs: -----------\" && tail /usr/local/apache/domlogs/*|grep -vE \"bytes|offsetftp|ftpxfer\"|grep \".\""
+cmd="echo \"----------- CTID $ctid IP $(vzlist $ctid -Ho ip) cPanel access logs: -----------\" && tail -n 50 /usr/local/apache/domlogs/*|grep -vE \"bytes|offsetftp|ftpxfer\"|grep \".\""
 
 vzctl exec $ctid $cmd 2>/dev/null >> /tmp/allvpslogscanneroutput
 
@@ -24,15 +24,59 @@ for ctid in $(vzlist -Ho ctid|grep -vE "$exclude");do
 
 if [ "$(vzctl exec $ctid ls /var/zpanel/logs/domains/zadmin/*access.log 2>/dev/null)" == "" ];then
 #echo "There is no such dir at $ctid, continue next iteration"
-#read fffff
 continue
 fi
 
-cmd="echo \"----------- CTID $ctid IP $(vzlist $ctid -Ho ip) zPanel access logs: -----------\" && tail /var/zpanel/logs/domains/zadmin/*access*|grep -vE \".png|.gif|.ico|.jpg|robots|GET / HTTP\""
+cmd="echo \"----------- CTID $ctid IP $(vzlist $ctid -Ho ip) zPanel access logs: -----------\" && tail -n 50 /var/zpanel/logs/domains/zadmin/*access*|grep -vE \".png|.gif|.ico|.jpg|robots|GET / HTTP\""
 
-vzctl exec $ctid $cmd >> /tmp/allvpslogscanneroutput
+vzctl exec $ctid $cmd 2>/dev/null >> /tmp/allvpslogscanneroutput
 
 # awk '{print $7}'
+
+done
+
+########### Nginx access logs
+
+for ctid in $(vzlist -Ho ctid|grep -vE "$exclude");do
+
+if [ "$(vzctl exec $ctid ls /var/log/nginx/*access.log* 2>/dev/null)" == "" ];then
+#echo "There is no such dir at $ctid, continue next iteration"
+continue
+fi
+
+cmd="echo \"----------- CTID $ctid IP $(vzlist $ctid -Ho ip) Nginx access logs: -----------\" && tail -n 50 /var/log/nginx/*access.log|grep -vE \".png|.gif|.ico|.jpg|robots|GET / HTTP\""
+
+vzctl exec $ctid $cmd 2>/dev/null >> /tmp/allvpslogscanneroutput
+
+done
+
+########### Webmin/Virtualmin access logs
+
+for ctid in $(vzlist -Ho ctid|grep -vE "$exclude");do
+
+if [ "$(vzctl exec $ctid ls /var/log/virtualmin 2>/dev/null)" == "" ];then
+#echo "There is no such dir at $ctid, continue next iteration"
+continue
+fi
+
+cmd="echo \"----------- CTID $ctid IP $(vzlist $ctid -Ho ip) Webmin/Virtualmin access logs: -----------\" && tail -n 50 /var/log/virtualmin/*|grep -vE \".png|.gif|.ico|.jpg|robots|GET / HTTP\""
+
+vzctl exec $ctid $cmd 2>/dev/null >> /tmp/allvpslogscanneroutput
+
+done
+
+########## Apache2 access logs (VestaCP..)
+
+for ctid in $(vzlist -Ho ctid|grep -vE "$exclude");do
+
+if [ "$(vzctl exec $ctid ls /var/log/apache2/domains 2>/dev/null)" == "" ];then
+#echo "There is no such dir at $ctid, continue next iteration"
+continue
+fi
+
+cmd="echo \"----------- CTID $ctid IP $(vzlist $ctid -Ho ip) Apache2 (VestaCP...) error logs: -----------\" && tail -n 50 /var/log/apache2/domains/*.error.log|grep -vE \".png|.gif|.ico|.jpg|robots|GET / HTTP\""
+
+vzctl exec $ctid $cmd 2>/dev/null >> /tmp/allvpslogscanneroutput
 
 done
 
@@ -42,15 +86,15 @@ for ctid in $(vzlist -Ho ctid|grep -vE "$exclude");do
 
 cmd="echo \"----------- CTID $ctid IP $(vzlist $ctid -Ho ip) Apache access logs: -----------\" && tail /var/log/httpd/access_log 2>/dev/null|grep -vE \"myadmin|MyAdmin|baidu\"| awk '{print $7}'|grep -vE \".png|.gif|.ico|.jpg\"|sort -u;tail /var/log/apache2/access.log 2>/dev/null|grep -vE \"myadmin|MyAdmin|baidu\"| awk '{print $7}'|grep -vE \".png|.gif|.ico|.jpg\"|sort -u"
 
-vzctl exec $ctid $cmd >> /tmp/allvpslogscanneroutput
+vzctl exec $ctid $cmd 2>/dev/null >> /tmp/allvpslogscanneroutput
 
 done
 
-########## Create webpage out of report file
+########## Create webpage out of logfile
 
-# Create WebPage Out Of Log File command
-cp -f /tmp/allvpslogscanneroutput /vz/root/MYVPSID/home/usernamehere/public_html/vps-logs.txt; vzctl exec MYVPSID chown usernamehere:usernamehere /home/usernamehere/public_html/vps-logs.txt
+# Create Web accessible page Out Of Log File (/tmp/allvpslogscanneroutput)
+cp -f /tmp/allvpslogscanneroutput /vz/root/MYOPENVZVPSID/home/MYCPANELUSERNAME/public_html/vpslogs.txt; vzctl exec MYOPENVZID chown MYCPANELUSERNAME:MYCPANELUSERNAME /home/MYCPANELUSERNAME/public_html/vpslogs.txt
 
-########## Send email to admin including report file
+########## Send email
 
-echo -e "Script /root/vpslogscanner ran at $(hostname) on $(date) and attached are access logs of all hosted VPSs except CTID $exclude \nView the logs on the web: http://admindomainhere.cz/vps-logs.txt" | mutt -a "/tmp/allvpslogscanneroutput" -s "VPSs access logs Report" -- $adminmail
+echo -e "Script /root/sh/vpslogscanner ran at $(hostname) on $(date) and attached are access logs of all hosted VPSs except CTID $exclude \nView the logs on the web: http://MYDOMAINNAME.cz/vpslogs.txt" | mutt -a "/tmp/allvpslogscanneroutput" -s "VPSs access logs Report" -- $adminmail
